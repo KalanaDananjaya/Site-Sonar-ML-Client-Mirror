26262e1fd427cd63802c2384b1b94e1f73e9f1ec
KalanaDananjaya <kalana.16@cse.mrt.ac.lk>
2020-07-17 18:36:23 +0530

    Add script to test database connection

---
+++ bin/TestDbConnection.java
@@ -0,0 +1,20 @@
+import lia.Monitor.Store.Fast.DB;
+
+public class TestDbConnection {
+
+    public static void main(String[] args) {
+        DB conn = new DB();
+        // Test database connectivity
+        String sql = "SELECT run_id FROM run ORDER BY run_id DESC LIMIT 1";
+        if (conn.query(sql)) {
+            System.err.println("Database Connection Successful");
+            String run_id = conn.gets(1);
+            String backend = conn.getBackend().getBackendName();
+            System.err.println("Established connection with "+ backend);
+            System.err.println("Latest run id: " + run_id);
+        }
+        else {
+            System.err.println("Unable to connect to database");
+        }
+    }
+}
